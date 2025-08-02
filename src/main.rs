22629efa005bc5a89cf18cc5ca3d4837dd0ec3ef
Kelechi Ebiri <ebiritg@gmail.com>
2025-08-02 23:47:19 +0100

    change cli option
    Signed-off-by: Kelechi Ebiri <ebiritg@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,24 @@ fn main() -> Result <(), Box<dyn Error>> {
 
     let commands: Vec<_> = std::env::args().collect();
 
-    if commands.len() < 4 {
-        eprintln!("Usage: huffit <argument> <input_file> <output_file>");
+    if commands.len() < 3 {
+        eprintln!("Usage: huffit <argument> <input_file>");
         exit(1);
     }
 
     let input_file = &commands[2];
-    let output_file = &commands[3];
 
     match commands[1].as_str() {
 
         "--compress" => {
-            if let Err(e) = encoder::encode(input_file.to_string(), output_file.to_string()) {
+            if let Err(e) = encoder::encode(input_file.to_string(), "compressed.txt".to_string()) {
                 eprintln!("Encoding error: {e}");
                 exit(1);
             }
         },
 
         "--decompress" => {
-            if let Err(e) = decoder::decode(input_file.to_string(), output_file.to_string()) {
+            if let Err(e) = decoder::decode(input_file.to_string(), "decompressed.txt".to_string()) {
                 eprintln!("Decoding error: {e}");
                 exit(1);
             }
